4a67d39875f6a130df3cbd962612047f3d228ddd
Ahmed-Benmassi <ahmedbenmassi@gmail.com>
2025-11-07 22:02:37 +0000

    bring tslm on the game

--- LSTM_model/LTSM.py
+++ LSTM_model/LTSM.py
@@ -1,3 +1,5 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 from influxdb_client import InfluxDBClient
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -27,5 +29,18 @@ def create_sequences(data, seq_length =10 ):
         x.append(data[i:i+seq_length])
         x.append(data[i+seq_length, 2])
     return np.array(x) ,np.array(y)
+
+seq_length= 10
+x,y =create_sequences(scaled_data,seq_length)
+train_size= int(len(x)*0.8)
+x_train, x_test=x[:train_size],x[:train_size:]
+y_train, y_test=y[:train_size],y[:train_size:]
+
+model =Sequential()
+model.add(LSTM(50,activation="relu",input_shape=(seq_length,x.shape[2])))
+model.add(Dense(1))
+model.compile(optimizer="adam", loss="mse")
+
+
        
 
